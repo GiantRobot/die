47c1fcb4ee11b40d3473e2a42a96b6cd6313fc9c
Chris Burgess <chris@giantrobot.co.nz>
2010-12-08 04:06:45 +0800

    Now has an Issue class which does more meaningful things.

--- die.rb
+++ die.rb
@@ -1,47 +1,125 @@
 #!/usr/bin/ruby
 
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'csv'
+
 module DrupalOrg
 
-  attr_accessor :nid
+  attr_accessor :nid, :pageurl
 
   @baseurl = 'http://drupal.org/'
   
-  def getUrl(loc)
+  # could this just be :pageurl as a custom accessor?
+  def get_url(loc)
     @pageurl = 'http://drupal.org/' + loc
     return @pageurl
   end
 
-  def getPage(loc)
-    p loc
-    return Nokogiri::HTML(open(self.getUrl(loc)).read)
+  # @todo: cache this
+  def get_page(loc)
+    return Nokogiri::HTML(open(self.get_url(loc)).read)
   end
   
 end
 
+# module Drupal_Node
+#
+#   include Drupal_Org
+#
+#  def get_path
+#    @path = 'node/' + @nid
+#  end
+#  
+# end
+
+# Issue stores details about a specific Drupal node of type Issue
 class Issue
 
   include DrupalOrg
 
+  attr_accessor :nid, :status, :title, :priority, :category, :version, :component, :comments, :comment_count, :last_updated, :assigned_username
+  
   def initialize(parameters)
     # i'd like to either be able to throw 
     # an ID *or* a set of details at this
     @nid = parameters['nid']
   end 
 
-  def loadPage
-    @page = self.getPage('node/'+@nid)
+  def get_url
+    super(self.get_path)
+  end
+
+  # As Issue is a subclass of Drupal Nodes, this could probably be moved
+  # to Drupal_Node class. Not doing that for now because I need to look at
+  # how super behaves when there are multiple parent classes
+  def get_path
+    @path = 'node/' + @nid
+  end
+  
+  def load_page
+    @page = self.get_page('node/'+@nid)
   end 
+  
+  def self.from_project_issues_row(row)
+    if (row/'td.views-field-title').inner_html.strip != ''
+      (row/'td.views-field-title a').map { |link|
+        issue        = Issue.new(link['href'].gsub('/node/','').to_s)
+        # p link['href'].gsub('/node/','').to_s
+        # p issue
+        # but issue.nid is not set in Issue.new("123")? 
+        # so i have to set it :(
+        issue.nid               = link['href'].gsub('/node/','').to_s
+        issue.status            = (row/'td.views-field-sid').inner_text.strip
+        issue.title             = (row/'td.views-field-title').inner_text.strip
+        issue.priority          = (row/'td.views-field-priority').inner_text.strip
+        issue.category          = (row/'td.views-field-category').inner_text.strip
+        issue.version           = (row/'td.views-field-version').inner_text.strip
+        issue.component         = (row/'td.views-field-component').inner_text.strip
+        issue.comment_count     = (row/'td.views-field-comment-count').inner_text.strip
+        issue.last_updated      = (row/'td.views-field-last-comment-timestamp').inner_text.strip
+        issue.assigned_username = (row/'td.views-field-name').inner_text.strip
+        return issue
+      }
+    end
+  end
+  
+  def self.csv_details_header
+    [
+      'Node ID',
+      'URL',
+      'Status',
+      'Title',
+      'Priority',
+      'Category',
+      'Version',
+      'Component',
+      'Comments',
+      'Updated',
+      'Assigned',          
+     ]
+  end
+  
+  def csv_details_row
+    [
+      @nid,
+      self.get_url,
+      @status,
+      @title,
+      @priority,
+      @category,
+      @version,
+      @component,
+      @comment_count,
+      @last_updated,
+      @assigned_username,    
+    ]
+  end
+  
+end  
 
-end
-
-# @todo 
-
-require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
-require 'csv'
-
-# I'm sure there's a nicer way to provide default args than if stmts 
+# I'm sure there's a nicer way to provide default args than if stmts?
 if !ARGV[0]
   puts "Please provide the project name as an argument, eg 'die.rb views'"
   exit
@@ -72,26 +150,8 @@ until issues.length == previous_length # stop when we run out of issues
   previous_length = issues.length
   (doc/'tr').each do |item|
     if (item/'td.views-field-title').inner_html.strip != ''
-      nid = '' # this shouldn't be important. I think it's here for voodoo.
-      (item/'td.views-field-title a').map { |link|
-        nid = link['href'].gsub('/node/','').to_s
-      }
-      # I want to tell it HOW to extract this information,
-      # this just tells it WHAT to do, that's not smart. 
-      issue = Issue.new({ 'nid' => nid })
-      #p issue.loadPage
-      issues[nid.to_s] = {
-        'nid' => nid,
-        'status' => (item/'td.views-field-sid').inner_text.strip,
-        'title' => (item/'td.views-field-title').inner_text.strip,
-        'priority' => (item/'td.views-field-priority').inner_text.strip,
-        'category' => (item/'td.views-field-category').inner_text.strip,
-        'version' => (item/'td.views-field-version').inner_text.strip,
-        'component' => (item/'td.views-field-component').inner_text.strip,
-        'comments' => (item/'td.views-field-comment-count').inner_text.strip,
-        'updated' => (item/'td.views-field-last-comment-timestamp').inner_text.strip,
-        'assigned' => (item/'td.views-field-name').inner_text.strip,
-      }
+      issue = Issue.from_project_issues_row(item)
+      issues[issue.nid.to_s] = issue
     end 
   end
   if issues.length > previous_length # could lose the until and just break out here anyway.
@@ -102,77 +162,25 @@ until issues.length == previous_length # stop when we run out of issues
   i += 1
 end
 
+# write the CSV out
 require "csv"
 if CSV.const_defined? :Reader
-	CSV::Writer.generate(outfile) do |csv|
-	  # again - I'd like to be saying, these are the values, this
-	  # is how to extract them, go do it ... Need to join the 
-	  # three blocks (extract info from table, csv headers,
-	  # csv rows) into something coherent.
-	  csv << [
-			  'Node ID',
-			  'URL',
-			  'Status',
-			  'Title',
-			  'Priority',
-			  'Category',
-			  'Version',
-			  'Component',
-			  'Comments',
-			  'Updated',
-			  'Assigned',          
-			 ]
-	  issues.each do |issue|
-		csv << [ 
-				issue[0],
-				'http://drupal.org/node/' + issue[0],
-				issue[1]['status'],
-				issue[1]['title'],
-				issue[1]['priority'],
-				issue[1]['category'],
-				issue[1]['version'],
-				issue[1]['component'],
-				issue[1]['comments'],
-				issue[1]['updated'],
-				issue[1]['assigned'],
-			   ]
-	  end
-	end
+  # old CSV::writer style pre ruby 1.9
+  CSV::Writer.generate(outfile) do |csv|
+    csv << Issue.csv_details_header
+    issues.each do |issue|
+      csv << issue[1].csv_details_row
+    end
+  end
 else
-	CSV.open(outfile, 'wb') do |csv|
-	  # again - I'd like to be saying, these are the values, this
-	  # is how to extract them, go do it ... Need to join the 
-	  # three blocks (extract info from table, csv headers,
-	  # csv rows) into something coherent.
-	  csv << [
-			  'Node ID',
-			  'URL',
-			  'Status',
-			  'Title',
-			  'Priority',
-			  'Category',
-			  'Version',
-			  'Component',
-			  'Comments',
-			  'Updated',
-			  'Assigned',          
-			 ]
-	  issues.each do |issue|
-		csv << [ 
-				issue[0],
-				'http://drupal.org/node/' + issue[0],
-				issue[1]['status'],
-				issue[1]['title'],
-				issue[1]['priority'],
-				issue[1]['category'],
-				issue[1]['version'],
-				issue[1]['component'],
-				issue[1]['comments'],
-				issue[1]['updated'],
-				issue[1]['assigned'],
-			   ]
-	  end
-	end
+  # it's ruby 1.9.x
+  CSV.open(outfile, 'wb') do |csv|
+    csv << Issue.csv_details_header
+    issues.each do |issue|
+      # p issue
+      csv << issue[1].csv_details_row
+    end
+  end
 end
 
 puts "Exported #{issues.length} issues to #{csvfilename}"
